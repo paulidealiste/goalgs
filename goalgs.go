0b3a900f6189d4256c012612e150fba2d8283645
paulidealiste <paulidealiste@aol.com>
2016-11-05 20:07:32 +0100

    Initial files and setup for go get

---
+++ goalgs.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/paulidealiste/goalgs/rangen"
+)
+
+func main() {
+	fmt.Println("Hello, goalgs!")
+	fmt.Println(rangen.Gaussrand())
+}
---
+++ rangen/rangen.go
@@ -0,0 +1,6 @@
+// Package rangen is a utility package for creating slices of Gaussian random numbers.
+package rangen
+
+func Gaussrand() string {
+	return "I'm randomly generated package ... in Gaussian!"
+}
